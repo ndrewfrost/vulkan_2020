0a1b7dcc2ec88bce5c0dbc9b6570395980577dde
Andrew <21962412+ndrewfrost@users.noreply.github.com>
2020-05-19 21:58:56 +0100

    Skeleton example vulkan

--- application/src/examplevulkan.h
+++ application/src/examplevulkan.h
@@ -18,4 +18,43 @@ public:
               uint32_t presentFamilyIdx,
               const vk::Extent2D& size);
 
+    void destroy();
+
+    void resize();
+
+    void rasterize();
+
+    void createDescripotrSetLayout();
+
+    void createGraphicsPipeline(const vk::RenderPass& renderPass);
+
+    void loadModel();
+
+    void updateDescriptorSet();
+
+    void createUniformBuffer();
+
+    void createSceneDescriptionBuffer();
+
+    void createTextureImages();
+
+    void updateUniformBuffer();
+
+
+    // Graphics Pipeline
+    vk::PipelineLayout      m_pipelineLayout;
+    vk::Pipeline            m_graphicsPipeline;
+
+    vk::DescriptorPool      m_descriptorPool;
+    vk::DescriptorSetLayout m_descPoolLayout;
+    vk::DescriptorSet       m_descriptorSet;
+
+    //app::Allocator          m_alloc;
+    vk::Device              m_device;
+    vk::PhysicalDevice      m_physicalDevice;
+    vk::Extent2D            m_size;
+    uint32_t                m_graphicsIdx{0};
+
+    // # Post
+
 }; // class ExampleVulkan
