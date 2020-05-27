de330020aa1f4db9bfa16ea95a478427ba7acc64
Andrew <21962412+ndrewfrost@users.noreply.github.com>
2020-05-27 12:09:55 +0100

    GraphicsPipelineGenerator skeleton

--- application/src/pipeline.hpp
+++ application/src/pipeline.hpp
@@ -12,4 +12,137 @@
 
 namespace app {
 
+///////////////////////////////////////////////////////////////////////////
+// GraphicsPipelineGenerator
+///////////////////////////////////////////////////////////////////////////
+
+struct GraphicsPipelineGenerator
+{
+public:
+    //--------------------------------------------------------------------------------------------------
+    //
+    //
+    GraphicsPipelineGenerator(const vk::Device &         device,
+                              const vk::PipelineLayout & layout,
+                              const vk::RenderPass &     renderPass) : device(device)
+    {
+       
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    //
+    //
+    GraphicsPipelineGenerator(GraphicsPipelineGenerator& pipelineGen)
+        : GraphicsPipelineGenerator(pipelineGen.device, pipelineGen.layout, pipelineGen.renderPass)
+    {
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    //
+    //
+    GraphicsPipelineGenerator& operator=(const GraphicsPipelineGenerator& other) = delete;
+
+    //--------------------------------------------------------------------------------------------------
+    //
+    //
+    ~GraphicsPipelineGenerator() 
+    { 
+        destroyShaderModules(); 
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    //
+    //
+    vk::PipelineShaderStageCreateInfo& addShader()
+    {
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    //
+    //
+    template <typename T>
+    vk::PipelineShaderStageCreateInfo& addShader()
+    {
+
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    //
+    //
+    vk::Pipeline create(const vk::PipelineCache& cache)
+    {
+
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    //
+    //
+    vk::Pipeline create()
+    {
+
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    //
+    //
+    void destroyShaderModules()
+    {
+
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    //
+    //
+    void update()
+    {
+    }
+
+private:
+    //--------------------------------------------------------------------------------------------------
+    //
+    //
+    void init()
+    {
+    }
+
+public:
+
+    //--------------------------------------------------------------------------------------------------
+    // Override Default pipeline associated structures
+    //
+    struct PipelineRasterizationStateCreateInfo
+    {};
+    struct PipelineInputAssemblyStateCreateInfo
+    {};
+    struct PipelineColorBlendAttachmentState
+    {};
+    struct PipelineColorBlendStateCreateInfo
+    {};
+    struct PipelineDynamicStateCreateInfo
+    {};
+    struct PipelineVertexInputStateCreateInfo
+    {};
+    struct PipelineViewportStateCreateInfo
+    {};
+    struct PipelineDepthStencilStateCreateInfo
+    {};
+
+    const vk::Device&                              device;
+    vk::GraphicsPipelineCreateInfo                 pipelineCreateInfo;
+    vk::PipelineCache                              pipelineCache;
+    vk::RenderPass&                                renderPass{ pipelineCreateInfo.renderPass };
+    vk::PipelineLayout&                            layout{ pipelineCreateInfo.layout };
+
+}; // struct GraphicsPipelineGenerator
+
+
+//--------------------------------------------------------------------------------------------------
+//
+//
+template <typename T>
+vk::PipelineShaderStageCreateInfo& app::GraphicsPipelineGenerator::addShader()
+{
+
+}
+
 } // namespace app
