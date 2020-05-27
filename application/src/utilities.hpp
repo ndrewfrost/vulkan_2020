799fb3afc926fad33b57441db6f8d881fc427c3b
Andrew <21962412+ndrewfrost@users.noreply.github.com>
2020-05-27 15:08:31 +0100

    read file

--- application/src/utilities.hpp
+++ application/src/utilities.hpp
@@ -16,5 +16,26 @@
 namespace app {
 namespace util {
 
+//--------------------------------------------------------------------------------------------------
+// Convenient function to load the shaders (SPIR-V)
+//
+inline std::vector<char> readFile(const std::string& filename)
+{
+    std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+    if (!file.is_open()) {
+        throw std::runtime_error("failed to open file!");
+    }
+
+    auto fileSize = static_cast<size_t>(file.tellg());
+    std::vector<char> buffer(fileSize);
+
+    file.seekg(0);
+    file.read(buffer.data(), fileSize);
+    file.close();
+
+    return buffer;
+}
+
 } // namespace util
 } // namespace app
